9f95b48cc5b4874f8926f3918ba4a4d82a90de4a
yuubd <gc.kim93@hotmail.com>
2018-07-24 17:53:34 -0700

    fix complile errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Navigation from './components/Navigation.js';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 class App extends Component {
@@ -16,4 +16,3 @@ class App extends Component {
 }
 
 export default App;
-Foodder;
