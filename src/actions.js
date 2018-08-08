258175cbe6c71272e319a3bd1434a25c29d81e43
yuubd <gc.kim93@hotmail.com>
2018-08-07 23:19:05 -0700

    action description added

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,7 @@
 import { CHANGE_SEARCH_FIELD } from './constants.js';
+
+// this is filetering placelist action
+// payload is whatever data needed to go on to the reducer
 export const setSearchField = (text) => ({
 	type: CHANGE_SEARCH_FIELD,
 	payload: text
