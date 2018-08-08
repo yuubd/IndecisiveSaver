2b7a08b0087cc48deb090ebe450b58758f6cc214
yuubd <gc.kim93@hotmail.com>
2018-08-07 23:18:25 -0700

    mapDispatchToProps and mapStateToProps added for connect() but type error..

--- src/App.js
+++ src/App.js
@@ -1,45 +1,58 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Navigation from './components/Navigation/Navigation';
-import PlaceList from './components/PlaceList/PlaceList.js';
-import SearchBox from './components/SearchBox/SearchBox.js';
-import { restaurants } from './components/Restaurant/restaurants';
-import Scroll from './components/Scroll/Scroll.js';
+import PlaceList from './components/PlaceList/PlaceList';
+import SearchBox from './components/SearchBox/SearchBox';
+import { restaurants } from './components/Restaurant/restaurants.js';
+import Scroll from './components/Scroll/Scroll';
 import './App.css';
 import { setSearchField } from './actions';
 
+// this can replace searchField in the state
 const mapStateToProps = (state) => {
+	console.log(state.searchRobots);
 	return {
-		searchField: state.searchRobots.searchField
+		//searchField: state.searchRobots.searchField
 	};
 };
+
+// dispatch is what triggers the actions
+// this can replace onSearchChane(event)
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	};
+};
+
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			restaurants: restaurants,
-			searchfield: ''
+			restaurants: restaurants
 		};
 	}
+
 	componentDidMount() {}
-	onSearchChange = (event) => {
-		this.setState({
-			searchfield: event.target.value
-		});
-	};
+
+	// onSearchChange = (event) => {
+	// 	this.setState({
+	// 		searchfield: event.target.value
+	// 	});
+	// };
 
 	render() {
-		const { restaurants, searchfield } = this.state;
+		const { restaurants } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredrestaurants = restaurants.filter((restaurants) => {
 			return restaurants.name
 				.toLowerCase()
-				.includes(searchfield.toLowerCase());
+				.includes(searchField.toLowerCase());
 		});
 
 		return (
 			<div className="pa0 ma0 width-middle">
 				<Navigation />
-				<SearchBox searchChange={this.onSearchChange} />
+				<SearchBox searchChange={onSearchChange} />
 				<Scroll>
 					<PlaceList restaurants={filteredrestaurants} />
 				</Scroll>
@@ -52,5 +65,6 @@ class App extends Component {
  * app knows there is redux store and interested in changes
  * what state should listen to = mapStateToProps, 
  * what action should listen to = mapDispatchToProps
+ * the returned objects are passed to App
  */
 export default connect(mapStateToProps, mapDispatchToProps)(App);
