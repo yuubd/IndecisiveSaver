7afbf04fbe3da55381457a389122a11230a1a222
yuubd <gc.kim93@hotmail.com>
2018-08-10 23:24:50 -0700

    connect parameters modified due to requestRestaurants() added

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,15 +3,18 @@ import { connect } from 'react-redux';
 import Navigation from '../components/Navigation/Navigation';
 import PlaceList from '../components/PlaceList/PlaceList';
 import SearchBox from '../components/SearchBox/SearchBox';
-import { restaurants } from '../components/Restaurant/restaurants';
+//import { restaurants } from '../components/Restaurant/restaurants';
 import Scroll from '../components/Scroll/Scroll';
 import './App.css';
-import { setSearchField } from '../actions';
+import { setSearchField, requestRestaurants } from '../actions';
 
 // this can replace searchField in the state
 const mapStateToProps = (state) => {
 	return {
-		searchField: state.searchField
+		searchField: state.searchPlaces.searchField,
+		restaurants: state.requestRestaurants.restaurants,
+		pending: state.requestRestaurants.isPending,
+		error: state.requestRestaurants.error
 	};
 };
 
@@ -19,19 +22,30 @@ const mapStateToProps = (state) => {
 // this can replace onSearchChane(event)
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRestaurants: () => dispatch(requestRestaurants())
 	};
 };
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			restaurants: restaurants
-		};
-	}
+	// constructor() {
+	// 	super();
+	// 	this.state = {
+	// 		restaurants: []
+	// 	};
+	// }
 
-	componentDidMount() {}
+	componentDidMount() {
+		// fetch('https://jsonplaceholer.typicode.com/users')
+		// 	.then((response) => response.json())
+		// 	.then((places) => {
+		// 		this.setState({ restaurants: places });
+		// 	});
+		// this.setState({
+		// 	restaurants: restaurants
+		// });
+		this.props.onRequestRestaurants();
+	}
 
 	onSearchChange = (event) => {
 		this.setState({
@@ -40,15 +54,15 @@ class App extends Component {
 	};
 
 	render() {
-		const { restaurants } = this.state;
-		const { searchField, onSearchChange } = this.props;
+		// const { restaurants } = this.state;
+		const { searchField, onSearchChange, restaurants, isPending } = this.props;
 		const filteredrestaurants = restaurants.filter((restaurants) => {
-			return restaurants.name
-				.toLowerCase()
-				.includes(searchField.toLowerCase());
+			return restaurants.name.toLowerCase().includes(searchField.toLowerCase());
 		});
 
-		return (
+		return isPending ? (
+			<h1>Loading </h1>
+		) : (
 			<div className="pa0 ma0 width-middle">
 				<Navigation />
 				<SearchBox searchChange={onSearchChange} />
