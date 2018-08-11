df587bebdba9e270d4c0a6d3ea5f178890da8f0f
yuubd <gc.kim93@hotmail.com>
2018-08-10 23:23:40 -0700

    requestRestaurant() added

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,24 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
-
+import {
+	CHANGE_SEARCH_FIELD,
+	REQUEST_RESTAURANTS_PENDING,
+	REQUEST_RESTAURANTS_SUCCESS,
+	REQUEST_RESTAURANTS_FAILED
+} from './constants.js';
+import { restaurants } from './components/Restaurant/restaurants';
 // this is filetering placelist action
 // payload is whatever data needed to go on to the reducer
 export const setSearchField = (text) => ({
 	type: CHANGE_SEARCH_FIELD,
 	payload: text
 });
+
+//
+export const requestRestaurants = () => (dispatch) => {
+	// dispatch({ type: REQUEST_RESTAURANTS_PENDING });
+	// fetch(restaurants)
+	// 	//.then((response) => response.json())
+	// 	.then((data) => dispatch({ type: REQUEST_RESTAURANTS_SUCCESS, payload: data }))
+	// 	.catch((error) => dispatch({ type: REQUEST_RESTAURANTS_FAILED, payload: error }));
+	dispatch({ type: REQUEST_RESTAURANTS_PENDING });
+	dispatch({ type: REQUEST_RESTAURANTS_SUCCESS, payload: restaurants });
+};
--- src/reducers.js
+++ src/reducers.js
@@ -1,17 +1,47 @@
-import { CHANGE_SEARCH_FIELD } from './constants.js';
+import {
+	CHANGE_SEARCH_FIELD,
+	REQUEST_RESTAURANTS_PENDING,
+	REQUEST_RESTAURANTS_SUCCESS,
+	REQUEST_RESTAURANTS_FAILED
+} from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
 	searchField: ''
 };
 
 // searchRobots should be a true function
 // return an output does not modify anything some input, same output
 // setting default parameter prevent errors when empty parameters passed
-export const searchPlaces = (state = initialState, action = {}) => {
+export const searchPlaces = (state = initialStateSearch, action = {}) => {
 	switch (action.type) {
 		case CHANGE_SEARCH_FIELD:
 			// merge state and then action.payload to an empty obj
 			return Object.assign({}, state, { searchField: action.payload });
+		default:
+			return state;
+	}
+};
+
+const initialStateRestaurants = {
+	isPending: false,
+	restaurants: [],
+	error: ''
+};
+
+export const requestRestaurants = (state = initialStateRestaurants, action = {}) => {
+	switch (action.type) {
+		case REQUEST_RESTAURANTS_PENDING:
+			return Object.assign({}, state, { isPending: true });
+		case REQUEST_RESTAURANTS_SUCCESS:
+			return Object.assign({}, state, {
+				restaurants: action.payload,
+				isPending: false
+			});
+		case REQUEST_RESTAURANTS_FAILED:
+			return Object.assign({}, state, {
+				error: action.payload,
+				isPending: false
+			});
 		default:
 			return state;
 	}
