6ed77f70f9bfdcdf85f2bc801573d04aeaf83dd6
yuubd <gc.kim93@hotmail.com>
2018-08-20 17:48:01 -0700

    converted SignIn component to a smart component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,60 +1,82 @@
 import React from 'react';
 
-const SignIn = ({ onRouteChange }) => {
-	return (
-		<main className="pa4 black-80">
-			<form className="measure center">
-				<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-					<legend className="f4 fw6 ph0 mh0">Sign In</legend>
-					<div className="mt3">
-						<label className="db fw6 lh-copy f6" htmlFor="email-address">
-							Email
+class SignIn extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			signInEmail: '',
+			signinPassword: ''
+		};
+	}
+	onEmailChange = (event) => {
+		this.setState({ signInEmail: event.target.value });
+	};
+	onPasswordChange = (event) => {
+		this.setState({ signinPassword: event.target.value });
+	};
+	onSubmitSignIn = () => {
+		this.props.onRouteChange('home');
+	};
+	render() {
+		const { onRouteChange } = this.props;
+		return (
+			<main className="pa4 black-80">
+				<form className="measure center">
+					<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+						<legend className="f4 fw6 ph0 mh0">Sign In</legend>
+						<div className="mt3">
+							<label className="db fw6 lh-copy f6" htmlFor="email-address">
+								Email
+							</label>
+							<input
+								className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+								type="email"
+								name="email-address"
+								id="email-address"
+								onChange={this.onEmailChange}
+							/>
+						</div>
+						<div className="mv3">
+							<label className="db fw6 lh-copy f6" htmlFor="password">
+								Password
+							</label>
+							<input
+								className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+								type="password"
+								name="password"
+								id="password"
+								onChange={this.onPasswordChange}
+							/>
+						</div>
+						<label className="pa0 ma0 lh-copy f6 pointer">
+							<input type="checkbox" /> Remember me
 						</label>
+					</fieldset>
+					<div className="">
 						<input
-							className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-							type="email"
-							name="email-address"
-							id="email-address"
+							onClick={this.onSubmitSignIn}
+							className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+							type="submit"
+							value="Sign in"
 						/>
 					</div>
-					<div className="mv3">
-						<label className="db fw6 lh-copy f6" htmlFor="password">
-							Password
-						</label>
-						<input
-							className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-							type="password"
-							name="password"
-							id="password"
-						/>
+					<div className="lh-copy mt3">
+						<a
+							onClick={() => onRouteChange('signUp')}
+							href="#0"
+							className="f6 link dim black db"
+						>
+							Sign up
+						</a>
+						<a href="#0" className="f6 link dim black db">
+							Forgot your password?
+						</a>
 					</div>
-					<label className="pa0 ma0 lh-copy f6 pointer">
-						<input type="checkbox" /> Remember me
-					</label>
-				</fieldset>
-				<div className="">
-					<input
-						onClick={() => onRouteChange('home')}
-						className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-						type="submit"
-						value="Sign in"
-					/>
-				</div>
-				<div className="lh-copy mt3">
-					<a
-						onClick={() => onRouteChange('signUp')}
-						href="#0"
-						className="f6 link dim black db"
-					>
-						Sign up
-					</a>
-					<a href="#0" className="f6 link dim black db">
-						Forgot your password?
-					</a>
-				</div>
-			</form>
-		</main>
-	);
-};
+				</form>
+			</main>
+		);
+	}
+}
+({ onRouteChange }) => {};
 
 export default SignIn;
