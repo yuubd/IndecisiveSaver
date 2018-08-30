6ee7829bcec2f3f1c6c78467399ca1438615b641
yuubd <gc.kim93@hotmail.com>
2018-08-29 21:16:31 -0700

    test

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -98,6 +98,8 @@ class App extends Component {
 		return (
 			<div>
 				{navigation}
+				{navigation}
+				{navigation}
 				{route === 'home' ? placeList : route === 'signUp' ? signUp : signIn}
 			</div>
 		);
