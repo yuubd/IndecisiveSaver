3b89bd9fd3104a3d84d9665d1b7f620fe75b1ff7
yuubd <gc.kim93@hotmail.com>
2018-08-31 23:17:23 -0700

    converted PlaceList component to a container

---
+++ src/containers/PlaceList/PlaceList.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Restaurant from '../../components/Restaurant/Restaurant';
+import { changeRoute } from '../../actions';
+
+const mapStateToProps = (state) => {
+	return {
+		route: state.changeRoute.route
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onRouteChange: (routeTo) => dispatch(changeRoute(routeTo))
+	};
+};
+
+class PlaceList extends React.Component {
+	restauantComponent = this.props.restaurants.map((restaurant, i) => {
+		const { onRouteChange } = this.props;
+		return (
+			<Restaurant
+				onClickChangeRoute={() => onRouteChange(i)}
+				key={restaurant.id}
+				name={restaurant.name}
+				imageURL={restaurant.imageURL}
+				visitedBy={restaurant.visited_by}
+				count={restaurant.count}
+				location={restaurant.location}
+				amount={restaurant.amount}
+				fullness={restaurant.fullnessfullness}
+				description={restaurant.description}
+			/>
+		);
+	});
+
+	render() {
+		const { route } = this.props;
+		return (
+			<ul className="pl0 pa0 ma0 flex flex-column justify-start list">
+				{this.restauantComponent}
+			</ul>
+		);
+	}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PlaceList);
