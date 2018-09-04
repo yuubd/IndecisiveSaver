a840e6d49d0c553fc777ff28fd0777150c4efbec
yuubd <gc.kim93@hotmail.com>
2018-09-03 22:10:43 -0700

    logo color and insa changed to truviews

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import hamburger from './hamburger.png';
+import './Logo.css';
 
 const Logo = () => {
 	return (
 		<ul className="pl0 flex flex-row justify-start list">
 			<img className="w-40 h-50" src={hamburger} alt="logo" />
-			<p className="f3 p3 mt2 fw8 light-yellow">InSa</p>
+			<p className="f1 p3 mt2 ma0 fw8 LogoStyl">Truviews</p>
 		</ul>
 	);
 };
 
 export default Logo;
+//-webkit-text-stroke-color: #f8d6b7;
+//-webkit-text-stroke-width: 1px;
