ec1e01fa0a23eba87e7019b9ac9dc5fe44fbcf06
yuubd <gc.kim93@hotmail.com>
2018-09-04 22:35:08 -0700

    miniNav added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 
-const Navigation = ({ onRouteChange, displayMenu }) => {
+const Navigation = (props) => {
 	const ulStyle = 'bg-washed-blue pa1 ma0 list flex flex-row justify-between';
 	const pStyle = 'f7 link dim black underline pa1 pointer fw5 mt0 dark-green';
-	return (
+	const main = (
 		<ul className={ulStyle}>
 			<li>
 				<Logo />
 			</li>
 			<li>
-				{displayMenu === 'home' ? (
-					<ul className="pa1 ma0 list flex flex-row">
-						<li>
-							<p className={pStyle}>Account</p>
-						</li>
-						<li>
-							<p onClick={() => onRouteChange('signIn')} className={pStyle}>
-								SignOut
-							</p>
-						</li>
-					</ul>
-				) : (
-					<p className={pStyle}>welcome!</p>
-				)}
+				<p className={pStyle}>welcome!</p>
 			</li>
 		</ul>
 	);
+	const miniNav = (
+		<ul className="bg-washed-blue pa1 ma0 list flex justify-end">
+			<li>
+				<p className={pStyle}>Account</p>
+			</li>
+			<li>
+				<p onClick={() => props.onRouteChange('signIn')} className={pStyle}>
+					SignOut
+				</p>
+			</li>
+		</ul>
+	);
+
+	return <div>{props.route === 'signIn' ? main : miniNav}</div>;
 };
 
 export default Navigation;
