44e7061473a9e5fe1371bbd4cb1d813675a1ac7e
yuubd <gc.kim93@hotmail.com>
2019-01-01 22:44:10 -0800

    refactored

--- src/components/RestaurantDetail/RestaurantDetail.js
+++ src/components/RestaurantDetail/RestaurantDetail.js
@@ -15,16 +15,6 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 class RestaurantDetail extends Component {
-	idx = this.getIndex(this.props.match.params.id);
-	key = this.props.restaurants[this.idx].id;
-	name = this.props.restaurants[this.idx].name;
-	imageURL = this.props.restaurants[this.idx].imageURL;
-	visitedBy = this.props.restaurants[this.idx].visited_by;
-	count = this.props.restaurants[this.idx].count;
-	location = this.props.restaurants[this.idx].location;
-	amount = this.props.restaurants[this.idx].amount;
-	description = this.props.restaurants[this.idx].description;
-
 	getIndex(id) {
 		const { restaurants } = this.props;
 		for (let i = 0; i < restaurants.length; i++) {
@@ -36,29 +26,36 @@ class RestaurantDetail extends Component {
 		return -1;
 	}
 
-	makeBurger(id) {
-		return (
-			<li key={id}>
-				<img className="pa0 ma0 bn mw1" src={hamburger} alt="ham" />
-			</li>
-		);
-	}
-
-	burgers(count) {
-		var burgerCount = count;
-		var burgersImg = [];
-		while (burgerCount > 0) {
-			burgersImg.push(this.makeBurger(burgerCount));
-			burgerCount--;
-		}
-		return burgersImg;
-	}
-
 	render() {
 		const { restaurants } = this.props;
-		console.log(this.props.match.params.id);
-		console.log('length: ' + restaurants.length);
-		console.log('id is : ' + restaurants[0].id);
+		const idx = this.getIndex(this.props.match.params.id);
+		const key = restaurants[idx].id;
+		const name = restaurants[idx].name;
+		const imageURL = restaurants[idx].imageURL;
+		const visitedBy = restaurants[idx].visited_by;
+		const count = restaurants[idx].count;
+		const location = restaurants[idx].location;
+		const amount = restaurants[idx].amount;
+		const description = restaurants[idx].description;
+
+		function makeBurger(id) {
+			return (
+				<li key={id}>
+					<img className="pa0 ma0 bn mw1" src={hamburger} alt="ham" />
+				</li>
+			);
+		}
+
+		function burgers(count) {
+			var burgerCount = count;
+			var burgersImg = [];
+			while (burgerCount > 0) {
+				burgersImg.push(makeBurger(burgerCount));
+				burgerCount--;
+			}
+			return burgersImg;
+		}
+
 		return (
 			<div>
 				<Link className="f6 lh-copy measure ma0 mr1 fr mid-gray pointer" to="/user/">
@@ -66,29 +63,29 @@ class RestaurantDetail extends Component {
 				</Link>
 				<article className="br2 dark-gray b--black-10 w-100 w-100-m w-25-l center">
 					<div className="dtc">
-						<h1 className="f-4 f4-ns mv0 dark-gray">{this.name}</h1>
-						<p className="f-2 lh-copy measure ma0 ml1 mid-gray">at {this.location}</p>
+						<h1 className="f-4 f4-ns mv0 dark-gray">{name}</h1>
+						<p className="f-2 lh-copy measure ma0 ml1 mid-gray">at {location}</p>
 					</div>
 					<img
-						src={require(`${this.imageURL}`)}
+						src={require(`${imageURL}`)}
 						className="db w-100 br2 br--top"
 						alt="{photo}"
 					/>
 					<div className="pa2 ph3-ns pb3-ns">
 						<div className="dt w-100 mt1">
 							<div className="dtc tr">
-								<h2 className="f5 mv0">${this.amount}</h2>
+								<h2 className="f5 mv0">${amount}</h2>
 							</div>
 						</div>
 						<ul className="flex list ma0 pa0">
 							<p className="f-2 lh-copy measure ma0 mid-gray">
-								visited by {this.visitedBy}
+								visited by {visitedBy}
 							</p>
 							<ul className="flex flex-row list pl0 padding-top-5 ma0 bn">
-								{this.burgers(this.count)}
+								{burgers(count)}
 							</ul>
 						</ul>
-						<p className="f6 lh-copy measure mt2 mid-gray">{this.description}</p>
+						<p className="f6 lh-copy measure mt2 mid-gray">{description}</p>
 					</div>
 				</article>
 			</div>
